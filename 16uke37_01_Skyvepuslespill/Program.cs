5b00ecb322ae91f5e1194cf02b1d6b9c1daa21bc
Mike Francis <mikefra@microsoft.com>
2023-01-02 17:56:37 -0800

    Update to .NET 6 LTS

--- 16uke37_01_Skyvepuslespill/Program.cs
+++ 16uke37_01_Skyvepuslespill/Program.cs
@@ -15,6 +15,7 @@ namespace WindowsFormsApplication1
         static void Main()
         {
             Application.EnableVisualStyles();
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new frmMenu());
         }
